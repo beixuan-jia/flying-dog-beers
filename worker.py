29292b32aea891bb1ebcfcd599e2295ded462663
beixuan-jia <54869709+beixuan-jia@users.noreply.github.com>
2020-05-06 20:23:34 -0400

    Add files via upload

---
+++ worker.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  6 16:41:11 2020
+
+@author: jiabx
+"""
+
+import os
+
+import redis
+from rq import Worker, Queue, Connection
+
+listen = ['high', 'default', 'low']
+
+redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+
+conn = redis.from_url(redis_url)
+
+if __name__ == '__main__':
+    with Connection(conn):
+        worker = Worker(map(Queue, listen))
+        worker.work()
